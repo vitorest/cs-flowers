df156cf9a7aed7edb5fd01635e5e362b26c081f8
Vítor Castro <64875903+vitorest@users.noreply.github.com>
2021-03-28 04:14:40 +0100

    scroll functions

--- src/Home.js
+++ src/Home.js
@@ -12,32 +12,49 @@ import Navlogo from './logo1.png';
 // import premio4 from './premio2018.png';
 // import premio5 from './premio2019.jpg';
 // import premio6 from './premioultimo.jpg';
-import { useRef } from 'react';
+import { Link } from "react-router-dom";
+
+
+
 
 function Home() {
-  // const executeScroll = () => scrollToRef(myRef);
-  const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
-  const myRef = useRef();
+
+    function handleScroll() {
+    window.scroll({
+      top: document.body.offsetHeight,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+      function handleScrollTwo() {
+    window.scroll({
+      top: 900,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 
   return (
     <div>
       <header>
         <img src={Navlogo} alt="" className="nav-logo" />
       </header>
-
       <div className="hero">
         <div className="hero-1"></div>
         <div className="hero-2">
           <nav>
             <ul>
               <li>
-                <button>Services</button>
+                <button onClick={handleScrollTwo}>Services</button>
               </li>
               <li>
-                <button>Galery</button>
+                < Link to="./gallery"><button>
+                  Gallery
+                  </button>
+                  </ Link>
               </li>
               <li>
-                <button>Find us</button>
+                <button onClick={handleScroll}>Find us</button>
               </li>
             </ul>
           </nav>
@@ -47,9 +64,8 @@ function Home() {
           <p>
             {/* !!!!!!!!!!!CORRIGIR ESTE INGLES MANHOSO!!!!!!!!! */}
             “CS Flowers Design is composed by a new and dynamic team witch main
-            goal is centered in turning your dreams becoming real. Creating
-            emotions in every moment. Focused on floral art, we work in general
-            decoration, fiancee bouquets and weeding accessories".
+            goal is centered in turning your dreams come true. Focused on floral art, our work is focused on general
+            decoration, fiancee bouquets and weeding accessories. We strive to create emotions in every moment!".
           </p>
           {/* A CS Flowers Design é composta por uma equipa jovem e dinâmica, cujo objectivo está centrado em TORNAR OS VOSSOS SONHOS REAIS...criando emoções em todos os momentos.
             Colocamos em todos os nossos trabalhos, amor e dedicação, para que o resultado seja simplesmente fantástico.
@@ -58,7 +74,7 @@ function Home() {
         <div className="hero-3"></div>
       </div>
 
-      <div className="services" ref={myRef}>
+      <div className="services" >
         <div>
           <div className="service-text">
             <div>WE GET YOUR BACK, ALWAYS FOCUSED ON YOUR DREAMS.</div>
@@ -78,19 +94,14 @@ function Home() {
         <div>
           <div className="service-text">
             <div>
-              WE HELP YOU OUT, TROUGHT A SPECIALIZED PLANNING TO YOUR WEDDING.
+              WE HELP YOU OUT TROUGHT A SPECIALIZED PLANNING FOR YOUR WEDDING.
             </div>
             {/* <h4>AJUDAMOS A REALIZA-LOS, ATRAVÉS DE UM PLANEAMENTO ESPECIALIZADO PARA O SEU CASAMENTO.</h4> */}
             <p>
               From the bouquet selection to your dream wedding party
-              organization, CS Flowers takes care of everything. Specialized in
-              wedding planning, we take care of all the details.
+              organization, CS Flowers takes care of everything to meet your expectations.
             </p>
-            <p>
-              desde a escolha do seu boquet até à organização da sua festa de
-              sonho, a cs flowers trata de tudo. especializados no planeamento
-              de casamentos, tratamos de todos os pormenores.
-            </p>
+
           </div>
           <div className="service-img">
             <img src={img2} alt="" />
@@ -117,8 +128,8 @@ function Home() {
             <p>
               Want to surprise someone that you love but dont have time? Dont
               worry! Our team takes care of everything. We idialize, create and
-              even personalize a message. Then? We deliver wherever you want. *
-              (consult prizes).
+              write down your message. Then? We deliver wherever you want. *
+              (consult prizes at store).
             </p>
             {/* <p>Quer surpreender alguem que ama mas não tem tempo? não se preocupe! A nossa equipa trata de tudo.
               Idealizamos, criamos e até personalizamos a sua mensagem. Depois? Entregamos na morada que desejar! * (preço consoante (varia) a localização</p> */}
